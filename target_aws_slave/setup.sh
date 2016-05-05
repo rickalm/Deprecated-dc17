8a8fbbd40b384b03a764ee075932d50155cb3dc4
CoreOS Admin <core@ip-10-250-4-200.us-west-2.compute.internal>
2016-05-05 00:36:44 +0000

    Slave Changes

--- target_aws_slave/setup.sh
+++ target_aws_slave/setup.sh
@@ -61,7 +61,7 @@ svc_remove_old_deps() {
 svc_needs_zookeeper() {
   svc_remove_old_deps $1
   svc_add_prestart $1	/opt/mesosphere/bin/wait_for_zookeeper.sh
-  svc_needs $1		dcos-exhibitor.service
+  #svc_needs $1		dcos-exhibitor.service
 }
 
 svc_needs_spartan() {
@@ -72,13 +72,13 @@ svc_needs_spartan() {
 
 svc_needs_leader() {
   svc_remove_old_deps $1
-  svc_needs $1		dcos-mesos-master.service
+  #svc_needs $1		dcos-mesos-master.service
   svc_add_prestart $1	/opt/mesosphere/bin/wait_till_ping.sh leader.mesos
 }
 
 svc_needs_marathon() {
   svc_remove_old_deps $1
-  svc_needs $1		dcos-marathon.service
+  #svc_needs $1		dcos-marathon.service
   svc_add_prestart $1	/opt/mesosphere/bin/wait_till_ping.sh marathon.mesos
 }
 
@@ -113,51 +113,24 @@ until /opt/mesosphere/bin/exhibitor_wait.py; do
 done
 EOF
 
-svc_rm_dep dcos-mesos-dns.service		dcos-mesos-master.service
-svc_needs_zookeeper dcos-mesos-dns.service
-
-svc_needs_zookeeper dcos-oauth.service
-
-svc_needs dcos-cluster-id.service		dcos-exhibitor.service
-svc_needs_zookeeper dcos-cluster-id.service
-
 svc_sed dcos-spartan.service			Pre=/ Pre=-/
 svc_needs dcos-spartan.service			dcos-epmd.service
-svc_needs dcos-spartan.service			dcos-exhibitor.service
 svc_starts dcos-spartan.service			dcos-gen-resolvconf.timer
 svc_starts dcos-spartan.service			dcos-spartan-watchdog.timer
 svc_needs_zookeeper dcos-spartan.service
 
-svc_needs_spartan dcos-spartan-watchdog.service
+#svc_needs_spartan dcos-spartan-watchdog.service
 svc_rm_dep dcos-spartan-watchdog.service	sleep.60
 
-svc_needs_spartan dcos-mesos-master.service
-svc_needs_clusterid dcos-mesos-master.service
-svc_needs dcos-mesos-master.service		dcos-mesos-dns.service
-
-svc_needs_leader dcos-marathon.service
-
 svc_needs_leader dcos-ddt.service
-
 svc_needs_leader dcos-cosmos.service
 
-svc_needs dcos-adminrouter.service		dcos-oauth.service
-svc_needs dcos-adminrouter.service		dcos-cosmos.service
-svc_needs dcos-adminrouter.service		dcos-ddt.service
-svc_needs dcos-adminrouter.service		dcos-history-service.service
-svc_cond_pathexists dcos-adminrouter.service	/opt/mesosphere/etc/adminrouter.env
-svc_cond_pathexists dcos-adminrouter.service	/opt/mesosphere/etc/dcos-oauth.env
-svc_cond_pathexists dcos-adminrouter.service	/var/lib/dcos/auth-token-secret
-svc_needs_marathon dcos-adminrouter.service
-svc_starts dcos-adminrouter.service		dcos-logrotate.timer
-svc_starts dcos-adminrouter.service		dcos-adminrouter-reload.timer
+svc_needs_leader dcos-mesos-slave.service
+svc_starts dcos-mesos-slave.service		dcos-ddt.service
+svc_starts dcos-mesos-slave.service		dcos-cosmos.service
+svc_starts dcos-mesos-slave.service		dcos-logrotate.timer
 
-#add_to_unit dcos-adminrouter-reload.service   RemainAfterExit yes
-#add_to_unit dcos-cluster-id.service           RemainAfterExit yes
-#add_to_unit dcos-gen-resolvconf.service       RemainAfterExit yes
-#add_to_unit dcos-logrotate.service            RemainAfterExit yes
-#add_to_unit dcos-spartan-watchdog.service     RemainAfterExit yes
-
+rm -rf /opt/mesosphere/active/spartan
 rm -rf /opt/mesosphere/active/minuteman
 rm -rf /opt/mesosphere/active/keepalived
 rm -rf /opt/mesosphere/active/dcos-signal
